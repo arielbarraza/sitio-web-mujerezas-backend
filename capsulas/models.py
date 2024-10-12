5a3891598d4725e0f9db336bc0cf63ec824690db
arielbarraza <ariel.barraza@ing.uchile.cl>
2024-10-12 12:29:43 -0300

    directorio archivos

--- capsulas/models.py
+++ capsulas/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Capsula(models.Model):
     titulo = models.CharField(max_length=300)
     descripcion = models.TextField(null=True, blank=True)
-    archivo = models.FileField()
+    archivo = models.FileField(upload_to="archivos/capsulas")
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     destacada = models.BooleanField(default=False)
--- noticias/models.py
+++ noticias/models.py
@@ -5,7 +5,7 @@ class Noticia(models.Model):
     titulo = models.CharField(max_length=300)
     bajada = models.TextField(null=True, blank=True)
     cuerpo = models.TextField()
-    imagen = models.ImageField(upload_to="imagenes/noticias")
+    imagen = models.ImageField(upload_to="archivos/noticias/imagenes")
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     destacada = models.BooleanField(default=False)
 
