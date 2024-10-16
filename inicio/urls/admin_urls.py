059662ba3add41dfd404616a672368f30811bfcb
arielbarraza <ariel.barraza@ing.uchile.cl>
2024-10-16 14:51:40 -0300

    endpoints manipulacion carrusel

--- inicio/urls/admin_urls.py
+++ inicio/urls/admin_urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from inicio.views import AdminCarruselInicioImagenCreateView, AdminCarruselInicioImagenListView
+from inicio.views import AdminCarruselInicioImagenCreateView, AdminCarruselInicioImagenListView, \
+    AdminCarruselInicioImagenRetrieveView, AdminCarruselInicioImagenUpdateView, AdminCarruselInicioImagenDestroyView
 
 urlpatterns = [
     path(r'todas/', AdminCarruselInicioImagenListView.as_view(), name=r'inicio_carrusel_admin_todas'),
-    #path(r'detalle/<pk>/', AdminNoticiasRetrieveView.as_view(), name=r'noticias_admin_detalle'),
+    path(r'detalle/<pk>/', AdminCarruselInicioImagenRetrieveView.as_view(), name=r'inicio_carrusel_admin_detalle'),
     path(r'anadir/', AdminCarruselInicioImagenCreateView.as_view(), name=r'inicio_carrusel_admin_anadir'),
-    #path(r'editar/<pk>/', AdminNoticiasUpdateView.as_view(), name=r'noticias_admin_editar'),
-    #path(r'eliminar/<pk>/', AdminNoticiasDestroyView.as_view(), name=r'noticias_admin_eliminar'),
+    path(r'editar/<pk>/', AdminCarruselInicioImagenUpdateView.as_view(), name=r'inicio_carrusel_admin_editar'),
+    path(r'eliminar/<pk>/', AdminCarruselInicioImagenDestroyView.as_view(), name=r'inicio_carrusel_admin_eliminar'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from inicio.models import CarruselInicioImagen
 from inicio.serializers.admin_serializers import CarruselInicioImagenAdminSerializer
@@ -8,5 +8,20 @@ class AdminCarruselInicioImagenListView(ListAPIView):
     serializer_class = CarruselInicioImagenAdminSerializer
     queryset = CarruselInicioImagen.objects.all().order_by('posicion')
 
+
+class AdminCarruselInicioImagenRetrieveView(RetrieveAPIView):
+    queryset = CarruselInicioImagen.objects.all()
+    serializer_class = CarruselInicioImagenAdminSerializer
+
+
 class AdminCarruselInicioImagenCreateView(CreateAPIView):
     serializer_class = CarruselInicioImagenAdminSerializer
+
+
+class AdminCarruselInicioImagenUpdateView(UpdateAPIView):
+    queryset = CarruselInicioImagen.objects.all()
+    serializer_class = CarruselInicioImagenAdminSerializer
+
+
+class AdminCarruselInicioImagenDestroyView(DestroyAPIView):
+    queryset = CarruselInicioImagen.objects.all()
